00dd2223942092fa7b6c7ef4d97a35e96bddfe2a
Ezequiel Chorolque <ezequielchorolque14@gmail.com>
2024-10-29 16:38:52 -0300

    redirect to main

--- src/app/layout/main/main.component.ts
+++ src/app/layout/main/main.component.ts
@@ -1,9 +1,20 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../header/header.component';
+import { FooterComponent } from '../footer/footer.component';
+import { AboutComponent } from '../../pages/about/about.component';
+import { ExperienceComponent } from '../../pages/experience/experience.component';
+import { SkillsComponent } from '../../pages/skills/skills.component';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [],
+  imports: [
+    HeaderComponent,
+    FooterComponent,
+    AboutComponent,
+    ExperienceComponent,
+    SkillsComponent
+  ],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
