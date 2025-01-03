80e0eacf652fd2d69344ced34e4e5a3a6bf31270
Ezequiel Chorolque <ezequielchorolque14@gmail.com>
2025-01-03 18:23:28 -0300

    refactor main and add badge

--- src/app/layout/main/main.component.ts
+++ src/app/layout/main/main.component.ts
@@ -6,6 +6,8 @@ import { ExperienceComponent } from '../../pages/experience/experience.component
 import { SkillsComponent } from '../../pages/skills/skills.component';
 import { ProjectsComponent } from "../../pages/projects/projects.component";
 import { ScrollService } from '../../core/services/scroll.service';
+import { BadgeComponent } from '../../shared/components/badge/badge.component';
+import { SvgIconComponent } from '../../shared/components/svg-icon/svg-icon.component';
 
 @Component({
   selector: 'app-main',
@@ -16,7 +18,9 @@ import { ScrollService } from '../../core/services/scroll.service';
     AboutComponent,
     ExperienceComponent,
     SkillsComponent,
-    ProjectsComponent
+    ProjectsComponent,
+    BadgeComponent,
+    SvgIconComponent
   ],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
@@ -24,6 +28,8 @@ import { ScrollService } from '../../core/services/scroll.service';
 export class MainComponent {
 
   @ViewChildren('section') sections!: QueryList<ElementRef>;
+  iconContact = `<svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-message-up"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M8 9h8" /><path d="M8 13h6" /><path d="M11.99 18.606l-3.99 2.394v-3h-2a3 3 0 0 1 -3 -3v-8a3 3 0 0 1 3 -3h12a3 3 0 0 1 3 3v5.5" /><path d="M19 22v-6" /><path d="M22 19l-3 -3l-3 3" /></svg>`
+  iconLinkedin = `<svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-brand-linkedin"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M8 11v5" /><path d="M8 8v.01" /><path d="M12 16v-5" /><path d="M16 16v-3a2 2 0 1 0 -4 0" /><path d="M3 7a4 4 0 0 1 4 -4h10a4 4 0 0 1 4 4v10a4 4 0 0 1 -4 4h-10a4 4 0 0 1 -4 -4z" /></svg>`
 
   constructor(private readonly scrollService: ScrollService) { }
 
