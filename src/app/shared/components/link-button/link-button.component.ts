342aac7a62b1b6b88edc89b2a6af1ef35ead223b
Ezequiel Chorolque <ezequielchorolque14@gmail.com>
2025-01-03 19:07:15 -0300

    change styles and add input for icons

--- src/app/shared/components/link-button/link-button.component.ts
+++ src/app/shared/components/link-button/link-button.component.ts
@@ -1,13 +1,15 @@
 import { Component, Input } from '@angular/core';
+import { SvgIconComponent } from '../svg-icon/svg-icon.component';
 
 @Component({
   selector: 'app-link-button',
   standalone: true,
-  imports: [],
+  imports: [SvgIconComponent],
   templateUrl: './link-button.component.html',
   styleUrl: './link-button.component.scss'
 })
 export class LinkButtonComponent {
-  @Input() href?: string;
-  @Input() text?: string;
+  @Input() href!: string;
+  @Input() text!: string;
+  @Input() icon!: string;
 }
